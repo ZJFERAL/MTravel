92693aa4115572b02bf15b304eea414c7b7cbf09
zhaojifeng <zhaojifengeral@163.com>
2017-04-19 18:15:42 +0800

    攻略模块 主页面展示完成

--- app/src/main/java/com/mcy/mtravel/adapter/PlanCountryAdapter.java
+++ app/src/main/java/com/mcy/mtravel/adapter/PlanCountryAdapter.java
@@ -1,11 +1,14 @@
 package com.mcy.mtravel.adapter;
 
 import android.content.Context;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.mcy.mtravel.R;
 import com.mcy.mtravel.entity.TargetPlaceBean;
 import com.zjf.core.adapter.CRecyclerViewAdapter;
 import com.zjf.core.adapter.CRecyclerViewViewHolder;
+import com.zjf.core.utils.DeviceUtils;
 
 import java.util.List;
 
@@ -15,14 +18,19 @@ import java.util.List;
 
 public class PlanCountryAdapter extends CRecyclerViewAdapter<TargetPlaceBean.DestinationsBean> {
 
+    private int widthPixels;
+
     public PlanCountryAdapter(Context context, List<TargetPlaceBean.DestinationsBean> data, int... itemLayoutIds) {
         super(context, data, itemLayoutIds);
+        widthPixels = DeviceUtils.getDeviceScreenWidth(context);
     }
 
     @Override
     public void setConvertView(CRecyclerViewViewHolder holder, TargetPlaceBean.DestinationsBean item, int position) {
         holder.setImageByUrl(R.id.img_bg, item.getImage_url(), R.drawable.weit_place)
                 .setText(R.id.txt_country_name, item.getName_zh_cn() + "\n" + item.getName_en())
-                .setText(R.id.txt_poi_num, item.getPoi_count() + "");
+                .setText(R.id.txt_poi_num, item.getPoi_count() + "旅行地");
+        ImageView view = holder.getView(R.id.img_bg);
+        view.setLayoutParams(new FrameLayout.LayoutParams(widthPixels / 2, (int) (widthPixels / 1.5)));
     }
 }
--- app/src/main/java/com/mcy/mtravel/adapter/PlanZoneAdapter.java
+++ app/src/main/java/com/mcy/mtravel/adapter/PlanZoneAdapter.java
@@ -1,7 +1,9 @@
 package com.mcy.mtravel.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.mcy.mtravel.R;
 import com.mcy.mtravel.entity.TargetPlaceBean;
@@ -24,6 +26,13 @@ public class PlanZoneAdapter extends CRecyclerViewAdapter<TargetPlaceBean> {
     public void setConvertView(CRecyclerViewViewHolder holder, TargetPlaceBean item, int position) {
         holder.setText(R.id.txt_zone_title, item.getCategory());
         RecyclerView view = holder.getView(R.id.recy_zone_item);
-        //view.setAdapter();
+        view.setLayoutManager(new GridLayoutManager(mContext, 2));
+        view.setAdapter(new PlanCountryAdapter(mContext, item.getDestinations(), R.layout.item_plan_country));
+        view.addOnItemTouchListener(new CRecyclerViewAdapter.RecyclerItemClickListener(mContext, new CRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+        }));
     }
 }
