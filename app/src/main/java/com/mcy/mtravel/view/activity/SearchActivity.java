d43698b4ad38461fc2f8d4b559817ce4c67b7536
zhaojifeng <zhaojifengeral@163.com>
2017-04-28 18:05:20 +0800

    搜索模块界面

--- app/src/main/java/com/mcy/mtravel/view/activity/SearchActivity.java
+++ app/src/main/java/com/mcy/mtravel/view/activity/SearchActivity.java
@@ -1,15 +1,47 @@
 package com.mcy.mtravel.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.mcy.mtravel.R;
+import com.mcy.mtravel.base.BaseActivity;
 
-public class SearchActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchActivity extends BaseActivity {
+
+    @BindView(R.id.title_search)
+    EditText mTitleSearch;
+    @BindView(R.id.img_search)
+    ImageView mImgSearch;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.tabLayout)
+    TabLayout mTabLayout;
+    @BindView(R.id.viewpager)
+    ViewPager mViewpager;
+    @BindView(R.id.coor_bg)
+    CoordinatorLayout mCoorBg;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void initVariables() {
+
+    }
+
+    @Override
+    public void initView() {
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void setListener() {
+
     }
 }
